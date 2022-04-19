573641fe72129293033ae9832393934cdc7235bb
Yogendra <ryogendrap@gmail.com>
2022-04-19 14:38:38 -0500

    Code

--- videz/routes/index.js
+++ videz/routes/index.js
@@ -11,4 +11,42 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "UTD!" });
 });
 
+
+/* GET home page. */
+router.get("/videos", function (req, res, next) {
+  collection.find({}, function (err, videos) {
+    if (err) throw err;
+    res.render('index', { videos });
+  });
+});
+
+router.get("/videos/new", function (req, res) {
+  res.render('new');
+});
+
+router.post("/videos", function (req, res, next) {
+  collection.insert(
+    /* req.body*/ {
+      title: req.body.title,
+      genre: req.body.genre,
+      description: req.body.description,
+      available: false
+    },
+    function (err, video) {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+      res.redirect('/videos');
+    }
+  );
+});
+
+router.get("/videos/:id", function (req, res, next) {
+  collection.findOne({ _id: req.params.id }, function (err, video) {
+    if (err) throw err;
+    res.render('show', { video });
+  });
+});
+
 module.exports = router;
